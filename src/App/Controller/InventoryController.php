4cc3c2281a55174553849100e54b521f9d10d54d
James Patrick Keegan <atomicbomber.email@gmail.com>
2016-11-16 08:17:03 +0700

    Added collapsible navbar to the inventory page

--- src/App/Controller/InventoryController.php
+++ src/App/Controller/InventoryController.php
@@ -7,8 +7,12 @@ class InventoryController extends BaseController
 {
     public function all ($request, $response)
     {
-
+        /* Attempt to retrieve the filter query from the URL s*/
         $filter = $request->getQueryParam("filter");
+
+        /* Bizarre PHP behavior where "0" is treated as a false value; Have to add an exception so it isn't
+            treated as so.
+        */
         if ( !$filter && $filter !== "0" ) { $filter = ""; }
 
         $items = Item::where("name", "LIKE", "%$filter%")
