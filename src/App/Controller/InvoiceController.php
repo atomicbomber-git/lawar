4e9a3c467782fb334bf7df17bbdaae43b4b9bd31
James Patrick Keegan <atomicbomber.email@gmail.com>
2016-11-22 11:17:16 +0700

    Now users can edit transaction items.

--- src/App/Controller/InvoiceController.php
+++ src/App/Controller/InvoiceController.php
@@ -5,6 +5,7 @@ use App\Model\TransactionItem;
 use App\Model\Transaction;
 use App\Model\Item;
 use Illuminate\Database\Capsule\Manager as Capsule;
+use Respect\Validation\Validator as V;
 
 class InvoiceController extends BaseController
 {
@@ -25,6 +26,7 @@ class InvoiceController extends BaseController
                     "transaction_items.name AS name",
                     "transaction_items.type AS type",
                     "transaction_items.size AS size",
+                    "transaction_items.price AS price",
                     "transaction_items.stock_warehouse AS stock_warehouse",
                     "transaction_items.stock_store AS stock_store",
                     "items.stock_warehouse AS orig_stock_warehouse",
@@ -44,7 +46,7 @@ class InvoiceController extends BaseController
                 items.stock_warehouse - transaction_items.stock_warehouse AS diff_stock_warehouse
             "))
             ->where("transaction_items.transaction_id", "=", $_SESSION["cart_id"])
-            ->havingRaw("diff_stock_store < 0 OR diff_stock_warehouse")
+            ->havingRaw("(diff_stock_store < 0) OR (diff_stock_warehouse < 0)")
             ->first();
 
         /* There's an error in our cart if there exists transaction items which quantity is larger than what's available in
@@ -60,16 +62,51 @@ class InvoiceController extends BaseController
         );
     }
 
-    public function cartFinish() {
+    public function cartFinish($request, $response) {
+
+
+        /* TODO: Check if invoice is valid */
+        
+        /* ---Substract all stock by the amount of purchased items--- */
+
+        /* Retrieve purchased items */
+        $transaction_items = TransactionItem::select("item_id", "stock_store", "stock_warehouse")
+            ->where("transaction_id", $_SESSION["cart_id"])
+            ->get();
+
+        foreach ($transaction_items as $transaction_item) {
+            /* Issue a query to substract stock items */
+            Item::find( $transaction_item->item_id )->decrement("stock_store", $transaction_item->stock_store );
+            Item::find( $transaction_item->item_id )->decrement("stock_warehouse", $transaction_item->stock_warehouse );
+
+            /* TODO: Store item transfer history */
+        }
+
+        /* Mark cart / transaction invoice as finished, so the next time the user log ins, she'll get a brand new empty cart */
         $transaction = Transaction::find($_SESSION["cart_id"]);
         $transaction->is_finished = true;
         $transaction->save();
+
+        /* Create a fresh new cart */
+        $cart = new Transaction([
+            "customer_name" => "",
+            "customer_phone" => "",
+            "datetime" => date("Y-m-d H-i-s"),
+            "clerk_id" => $_SESSION["user_id"],
+            "is_finished" => 0
+        ]);
+
+        $cart->save();
+
+        /* Save cart id into session so the site knows which cart we're using currently */
+        $_SESSION["cart_id"] = $cart->id;
+        return $response->withStatus(302)->withHeader("Location", $this->router->pathFor("inventory"));
     }
 
     public function addTransactionItem ($request, $response, $args)
     {
         $item = Item::find($args["item_id"]);
-        return $this->view->render($response, "invoice/add_transaction_item.twig", ["item" => $item]);
+        return $this->view->render($response, "invoice/transaction_item_add.twig", ["item" => $item]);
     }
 
     public function processAddTransactionItem ($request, $response, $args)
@@ -120,6 +157,62 @@ class InvoiceController extends BaseController
     {
         $transaction_item = TransactionItem::find($args["item_id"]);
         $transaction_item->delete();
+        return $response->withStatus(302)->withHeader("Location", $this->router->pathFor("cart"));
+    }
+
+    public function editTransactionItem ($request, $response, $args) {
+
+        /* Retrieve messages that were stored in the session */
+        $message = null;
+        if ( isset($_SESSION["message"] ) ) {
+            $message = $_SESSION["message"];
+            unset( $_SESSION["message"] );
+        }
+
+        $transaction_item = TransactionItem::leftJoin("items", "transaction_items.item_id", "=", "items.id")
+            ->select(
+                "transaction_items.id AS id",
+                "transaction_items.item_id AS item_id",
+                "transaction_items.transaction_id AS transaction_id",
+                "transaction_items.name AS name",
+                "transaction_items.type AS type",
+                "transaction_items.size AS size",
+                "transaction_items.price AS price",
+                "transaction_items.stock_warehouse AS stock_warehouse",
+                "transaction_items.stock_store AS stock_store",
+                "items.stock_warehouse AS orig_stock_warehouse",
+                "items.stock_store AS orig_stock_store"
+            )
+            ->where("transaction_items.id", $args["item_id"])
+            ->first();
+
+        return $this->view->render($response, "invoice/transaction_item_edit.twig", ["item" => $transaction_item, "message" => $message]);
+    }
+
+    public function processEditTransactionItem ($request, $response, $args)
+    {
+    
+        $data = $request->getParsedBody();
+
+        /* Validate */
+        $hasError = false;
+        if ( ! V::intVal()->min(0)->validate($data["stock_store"]) ) {
+            $_SESSION["message"]["error"]["stock_store"] = "Error: Nilai wajib positif";
+            $hasError = true;
+        }
+
+        if ( ! V::intVal()->min(0)->validate($data["stock_warehouse"]) ) {
+            $_SESSION["message"]["error"]["stock_warehouse"] = "Error: Nilai wajib positif";
+            $hasError = true;
+        }
+
+        if ( $hasError ) {
+            return $response->withStatus(302)->withHeader("Location", $this->router->pathFor("invoice-item-edit", ["item_id" => $args["item_id"]]));
+        }
+
+        $transaction_item = TransactionItem::find($args["item_id"]);
+        $transaction_item->update( $request->getParsedBody() );
+
         return $response->withStatus(302)->withHeader("Location", $this->router->pathFor("cart"));
     }
 }
